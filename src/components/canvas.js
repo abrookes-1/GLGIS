b6f92b6929dc7f4eae409eef4a44fc9b6a856b6b
Aidan Brookes <abrookes@mines.edu>
2021-02-13 18:37:24 -0700

    TRIANGLE!

---
+++ src/components/canvas.js
@@ -0,0 +1,127 @@
+import React, { useState, useEffect, useRef } from 'react';
+
+var vertexShaderText =
+[
+'precision mediump float;',
+'',
+'attribute vec2 vertPosition;',
+'attribute vec3 vertColor;',
+'varying vec3 fragColor;',
+'',
+'void main()',
+'{',
+'  fragColor = vertColor;',
+'  gl_Position = vec4(vertPosition, 0.0, 1.0);',
+'}'
+].join('\n');
+
+var fragmentShaderText =
+[
+'precision mediump float;',
+'',
+'varying vec3 fragColor;',
+'void main()',
+'{',
+'  gl_FragColor = vec4(fragColor, 1.0);',
+'}'
+].join('\n');
+
+function GlCanvas() {
+  const canvasRef = useRef(null);
+
+  // function vertexShader(vertPosition, vertColor){
+  //   return {
+  //     fragColor: vertColor,
+  //     gl_Position: [vertPosition.x, vertPosition.y, 0.0, 1.0]
+  //   }
+  // }
+
+  useEffect(() => {
+    // alert("effect");
+    let canvas = canvasRef.current // TODO: ensure component type is canvas
+    let gl = canvas.getContext('webgl');
+
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    gl.viewport(0, 0, window.innerWidth, window.innerHeight);
+
+    gl.clearColor(0.75, 0.85, 0.8, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+    // create shaders
+    let vertexShader = gl.createShader(gl.VERTEX_SHADER);
+    let fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+
+    gl.shaderSource(vertexShader, vertexShaderText);
+    gl.shaderSource(fragmentShader, fragmentShaderText);
+
+    gl.compileShader(vertexShader);
+    // check for compile errors in shaders
+    if (!gl.getShaderParameter(vertexShader, gl.COMPILE_STATUS)) {
+      console.error('ERROR compiling vertex shader!', gl.getShaderInfoLog(vertexShader));
+      return;
+    }
+
+    gl.compileShader(fragmentShader);
+    // check for compile errors in shaders
+    if (!gl.getShaderParameter(fragmentShader, gl.COMPILE_STATUS)) {
+      console.error('ERROR compiling fragment shader!', gl.getShaderInfoLog(fragmentShader));
+      return;
+    }
+
+    let program = gl.createProgram();
+    gl.attachShader(program, vertexShader);
+    gl.attachShader(program, fragmentShader);
+    gl.linkProgram(program);
+    // check for linker errors
+    if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
+      console.error('ERROR linking program!', gl.getProgramInfoLog(program));
+    }
+    // TODO: validate in debug only
+    gl.validateProgram(program);
+    if (!gl.getProgramParameter(program, gl.VALIDATE_STATUS)) {
+      console.error('ERROR validating program!', gl.getProgramInfoLog(program));
+      return;
+    }
+
+    //
+    // create buffer
+    //
+    var triangleVertices = [ // x, y points in counter-clockwise order
+      0.0, 0.5,
+      -0.5, -0.5,
+      0.5, -0.5
+    ];
+
+    var triangleVertexBufferObj = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, triangleVertexBufferObj);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(triangleVertices), gl.STATIC_DRAW);
+
+    var positionAttribLocation = gl.getAttribLocation(program, 'vertPosition');
+    gl.vertexAttribPointer(
+      positionAttribLocation,
+      2, // number of elements per attribute
+      gl.FLOAT,
+      gl.FALSE, // whether data is normalized
+      2 * Float32Array.BYTES_PER_ELEMENT, // size of an individual vertex
+      0 // offset from the beginning of a single vertex to this attribute
+    );
+
+    gl.enableVertexAttribArray(positionAttribLocation);
+
+    //
+    // Main render loop
+    //
+    gl.useProgram(program);
+    gl.drawArrays(gl.TRIANGLES, 0, 3);
+  })
+
+
+  return (
+    <canvas ref={canvasRef} id={"game-surface"} width={800} height={600}>
+      Browser does not support HTML5.
+    </canvas>
+  );
+}
+
+export { GlCanvas }
